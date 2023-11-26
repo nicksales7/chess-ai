e72ae5259cf0af374b51736e0912c1516cbd3df7
nicksales7 <nicksales72@gmail.com>
2023-11-26 06:32:52 -0500

    less throwing

--- move.py
+++ move.py
@@ -19,12 +19,27 @@ class ChessGame:
         made_move = self.board.push_san(move)
         return made_move
 
+    def game_over(self):
+        if (
+            self.board.is_checkmate()
+            or self.board.is_stalemate()
+            or self.board.has_insufficient_material(self.color)
+            or self.board.is_seventyfive_moves()
+            or self.board.is_fivefold_repetition()
+        ):
+            return True
+        else:
+            return False
+
 
 if __name__ == "__main__":
-    # using below functions to test funcionality and see what happens, not final logic
     game = ChessGame(True)
-    legal_moves = game.legal_moves()
-    board = game.board
-    print(board)
-    game.make_move("e4")
-    print(board)
+    game_over = False
+
+    while not game_over:
+        # Game logic
+
+        if game.game_over():
+            game_over = True
+            print(game.board)
+        ### Main game logic to be done below
